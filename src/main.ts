7272a761108bdc55fffd1182215f2d34144191b0
iacopo ciao <iacopociao1992@gmail.com>
2020-04-29 16:51:40 +0200

    added class validator

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import 'reflect-metadata';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -17,6 +18,8 @@ async function bootstrap() {
 	const swaggerDocument = SwaggerModule.createDocument(app, options);
 
 	SwaggerModule.setup('api', app, swaggerDocument);
+
+	app.useGlobalPipes(new ValidationPipe({transform: true}));
 
 	await app.listen(3000);
 
